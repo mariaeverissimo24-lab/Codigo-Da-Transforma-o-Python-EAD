ebaf1f3155d4a441136e9fd049779f47d5d6bad1
mariaeverissimo24-lab <mariaeverissimo24@gmail.com>
2026-08-19 16:24:42 -0300

    upgrade

---
+++ Modulo1/none.py
@@ -0,0 +1,9 @@
+def calcular_media(lista_numeros):
+    if not lista_numeros:
+        return sum(lista_numeros)\ len(lista_numero)
+    
+    def e_par(numero):
+        return numero % 2 == 0
+    def divisao_inteira (a,b): 
+        """
+        Retorna
